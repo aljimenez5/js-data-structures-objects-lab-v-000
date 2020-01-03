a7158a69aef1c480dea872615b98b03a0da1e636
Arvida Jimenez <arvida.jimenez@gmail.com>
2020-01-03 20:52:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,3 +3,7 @@ const driver = { person: "drives" }
 function updateDriverWithKeyAndValue(driver, key, value) {
    return Object.assign({}, driver, {[key]: value})
 }
+
+function destruvtivelyUpdateDriverWithKeyAndValue(driver, key, value) {
+  
+}
